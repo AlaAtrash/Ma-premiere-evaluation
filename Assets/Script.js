fc0df9f3b5c0d116ad015d7a265d59c8256e0501
Sarah Bendavid <sarah.laura.bendavid@gmail.com>
2023-02-27 13:22:15 +0200

    Reorganisation du dossier

---
+++ Assets/Script.js
@@ -0,0 +1,9 @@
+console.log ("test");
+
+let btn = document.getElementById ('btnAddBeneficiaire')
+
+function evenement () {
+    alert ('Nouvelle inscription')
+}
+
+btn.addEventListener('click', evenement);
